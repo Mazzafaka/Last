71d551e941fb5995f33b87042e987d00f61e0b1e
Zack <1984odina.gmail.com>
2023-08-04 16:50:18 +0600

    Added all

---
+++ lst_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void Main()
+{
+    Console.Write("Введите значение N: ");
+    int N = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение M: ");
+    int M = int.Parse(Console.ReadLine());
+    int numbers = PrintNumbers(N, M);
+
+    Console.WriteLine($"Сумма натуральных чисел M и N: {numbers}");
+}
+
+int PrintNumbers(int first, int last)
+{
+    if (first > last)
+        {
+            return 0;
+        }
+        return first + PrintNumbers(first + 1, last);
+}
+
+Main();
---
+++ lst_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+void Main()
+{
+    Console.Write("Введите значение m: ");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите значение n: ");
+    int n = int.Parse(Console.ReadLine());
+
+    int result = AckermannFunction(m, n);
+    Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
+}
+
+int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else if (m > 0 && n > 0)
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+        else
+        {
+            throw new ArgumentException("Значения m и n должны быть неотрицательными.");
+        }
+    }
+
+Main();
